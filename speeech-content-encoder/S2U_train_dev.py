c4a0e8253a9a822465b9930c4e95b48953016a1b
Jaeseok Yoon <jake.yoon@kaist.ac.kr>
2022-07-19 16:59:06 +0900

    Update SCE implementation for dstc

--- speeech-content-encoder/S2U_train_dev.py
+++ speeech-content-encoder/S2U_train_dev.py
@@ -1,14 +1,32 @@
+import h5py
 import numpy as np
 import joblib
-import torch
-import torchaudio 
-import pandas as pd
+import torchaudio
 from tqdm import tqdm
-import os 
+import os
 import torch
+import glob
 
 SAMPLE_RATE = 16000
 CHUNK_LENGTH = 250000
+
+tags = ['line_nr', 'dialog_id', 'turn_id']
+
+
+def parse_key(key_str):
+    data = {}
+    for idx, tag in enumerate(tags):
+        value = ''
+        if key_str.find(tag) >= 0:
+            if idx < len(tags) - 1:
+                value = key_str[key_str.find(tag) + len(tag) + 1:key_str.find(tags[idx + 1])]
+            else:
+                value = key_str[key_str.find(tag) + len(tag) + 1:]
+            value = value.strip()
+        data[tag] = value
+    return data
+
+
 class ApplyKmeans(object):
     def __init__(self, km_path, return_diff=False):
         self.km_model = joblib.load(km_path)
@@ -44,76 +62,100 @@ class ApplyKmeans(object):
             else:
                 return np.argmin(dist, axis=1)
 
+
+tmp_file = 'tmp.wav'
+
+
 def reader(fname):
-    wav, ori_sr = torchaudio.load(fname)
-    if ori_sr != SAMPLE_RATE:
-        wav = torchaudio.transforms.Resample(ori_sr, SAMPLE_RATE)(wav)
-    return wav.squeeze()
+    wav_list = []
+    try:
+        hd5_file = h5py.File(fname, 'r')
+    except OSError:
+        print(f'{fname} file is abnormal')
+        return wav_list
+
+    for key in hd5_file.keys():
+        wav = hd5_file[key]['audio'][:]
+        parsed_key = parse_key(str(key))
+
+        # create temporary wav file to get torch audio vector
+        import soundfile as sf
+        sf.write(file=tmp_file, data=wav, samplerate=16000)
+        wav, ori_sr = torchaudio.load(tmp_file)
+
+        if ori_sr != SAMPLE_RATE:
+            wav = torchaudio.transforms.Resample(ori_sr, SAMPLE_RATE)(wav)
+        os.remove(path=tmp_file)
+        wav_list.append({
+            'wav': wav.squeeze(),
+            'key': parsed_key['line_nr'] + '_' + parsed_key['dialog_id'].removesuffix('.json') + '_' + parsed_key['turn_id']
+        })
+    hd5_file.close()
+    return wav_list
 
 
-# train
-df = pd.read_csv('/home/daniel094144/E2E-SpokenQA/meta-train.csv')
-audio_file_dir = '/home/daniel094144/E2E-SpokenQA/train_audios/'
 
-output_dir = '/home/daniel094144/data/SQA_code/hubert_large_128/train_code'
-extractor = torch.hub.load('s3prl/s3prl', 'hubert_large_ll60k')    
-extractor.eval()
-if torch.cuda.is_available():
+
+train_folders = ['tpa', 'tpb', 'tpc', 'tpd']
+for folder in train_folders:
+    print(f'{folder} start')
+
+    # train
+    train_file_list = glob.glob(f'data/base/train/{folder}/*.hd5')
+
+    output_dir = f'data/SQA_code/train/{folder}'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir, exist_ok=True)
+    extractor = torch.hub.load('s3prl/s3prl', 'hubert_large_ll60k')
+    extractor.eval()
     extractor = extractor.cuda()
-apply_kmeans = ApplyKmeans('/home/daniel094144/Daniel/DUAL-textless-SQA/hubert-cluster-code/km_100h_c128/km_feat_layer_22')
 
+    apply_kmeans = ApplyKmeans(
+        'ext/DUAL-textless-SQA/speeech-content-encoder/km_100h_c128/km_feat_layer_22')
 
-for file in tqdm(df['id'].values, desc='transforming passage to discrete code'):
-    audio_file = os.path.join(audio_file_dir, file+'.mp3')
-    wavs = reader(audio_file)
+    for train_file in tqdm(train_file_list, desc='transforming passage to discrete code'):
+        file = os.path.basename(train_file)
+        waves = reader(train_file)
 
-    if len(wavs) > 20 * SAMPLE_RATE: 
-        continue
-    
-    wavs = wavs.cuda()    
-    feature = extractor([wavs])    
+        for idx, wav in enumerate(waves):
+            if len(wav['wav']) > 20 * SAMPLE_RATE:
+                continue
 
-    
-    code = apply_kmeans(feature['hidden_state_22'].squeeze().cuda())
-    code = torch.tensor(code)
+            feature = extractor([wav['wav'].cuda()])
 
-    merged_code, counts = torch.unique_consecutive(code, return_counts=True)
-    np.savetxt(os.path.join(output_dir, file+'.code'), merged_code.long(), fmt='%i')    
-    np.savetxt(os.path.join(output_dir, file+'.cnt'), counts.long(), fmt='%i')
+            code = apply_kmeans(feature['hidden_state_22'].squeeze().cuda())
+            code = torch.tensor(code)
+
+            merged_code, counts = torch.unique_consecutive(code, return_counts=True)
+            np.savetxt(os.path.join(output_dir, f'{wav["key"]}.code'), merged_code.long(), fmt='%i')
+            np.savetxt(os.path.join(output_dir, f'{wav["key"]}.cnt'), counts.long(), fmt='%i')
+
+    print(f'{folder} end')
 
 # dev
-df = pd.read_csv('/home/daniel094144/E2E-SpokenQA/meta-dev.csv')
-audio_file_dir = '/home/daniel094144/E2E-SpokenQA/dev_audios/'
 
-output_dir = '/home/daniel094144/data/SQA_code/hubert_large_128/dev_code'
+output_dir = 'data/SQA_code/dev'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir, exist_ok=True)
 
+dev_file_list = glob.glob('data/base/dev/*.hd5')
 
-for file in tqdm(df['id'].values, desc='transforming passage to discrete code'):
-    audio_file = os.path.join(audio_file_dir, file+'.mp3')
-    wavs = reader(audio_file)
-    wavs = wavs.cuda()   
+print('dev folder start')
+for dev_file in tqdm(dev_file_list, desc='transforming passage to discrete code'):
+    file = os.path.basename(dev_file)
+    waves = reader(dev_file)
 
-    if len(wavs) > 20 * SAMPLE_RATE: 
-        print(f'{file} too long')
-        chunks = torch.split(wavs, CHUNK_LENGTH) 
-        for i, chunk in enumerate(chunks): 
-            feat = extractor([chunk])
-            feat = feat['hidden_state_22'].squeeze()
-            
-            if i == 0:
-                feature = feat
-            else: 
-                feature = torch.cat([feature, feat], dim = 0)
+    for idx, wav in enumerate(waves):
 
-        code = apply_kmeans(feature.cuda())
+        if len(wav['wav']) > 20 * SAMPLE_RATE:
+            print(f'wav is too long {dev_file}_{idx}')
+            continue
 
-    else:
-        feature = extractor([wavs])    
+        feature = extractor([wav['wav'].cuda()])
 
         code = apply_kmeans(feature['hidden_state_22'].squeeze().cuda())
+        code = torch.tensor(code)
 
-    code = torch.tensor(code)
-
-    merged_code, counts = torch.unique_consecutive(code, return_counts=True)
-    np.savetxt(os.path.join(output_dir, file+'.code'), merged_code.long(), fmt='%i')    
-    np.savetxt(os.path.join(output_dir, file+'.cnt'), counts.long(), fmt='%i')
+        merged_code, counts = torch.unique_consecutive(code, return_counts=True)
+        np.savetxt(os.path.join(output_dir, f'{wav["key"]}.code'), merged_code.long(), fmt='%i')
+        np.savetxt(os.path.join(output_dir, f'{wav["key"]}.cnt'), counts.long(), fmt='%i')
